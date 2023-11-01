14cec34ac9b5bf92d91ba37888eafd2e6abdc6b7
William Chien <williamchien@Williams-MacBook-Pro-3.local>
2023-11-01 15:25:43 +0800

    update-to-store

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,6 +2,7 @@ import getBillboard from '@/actions/get-billboard';
 import getProducts from '@/actions/get-products';
 import ProductList from '@/components/product-list';
 import Container from '@/components/ui/Container';
+import Link from 'next/link';
 import Billboard from '@/components/ui/billboard';
 import React from 'react'
 export const revalidate = 0; 
@@ -11,7 +12,10 @@ const HomePage = async() => {
   return (
     <Container>
       <div className="space-y-10 pb-10 ">
+     
+        <a href='https://www.facebook.com/profile.php?id=100090955283942'>
         <Billboard data={billboard}/>
+        </a>
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
         </div>
